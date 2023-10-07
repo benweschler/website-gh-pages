78db6999a836863ad4ee6e9a6bec95617ef09a2d
Ben Weschler <91393341+benweschler@users.noreply.github.com>
2023-10-07 12:16:35 -0700

    Added mobile & small window size support

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "980547175e325fe622a3362b84d55b6a",
-"index.html": "a82f23344b06bf857942dca4b8debf20",
-"/": "a82f23344b06bf857942dca4b8debf20",
-"main.dart.js": "c73b85dce75e74dc6a7bb882f0111cbc",
+"index.html": "563887a8a4e9d7e68138808f24541946",
+"/": "563887a8a4e9d7e68138808f24541946",
+"main.dart.js": "cf3016d57e9b20c7abdbab9db9125398",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
